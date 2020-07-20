8d938940fb055523cebc64fa2a2ca4a43f6237bc
persdre <persdre@gmail.com>
2020-07-20 20:45:19 +0800

    add comment section

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -272,8 +272,12 @@ class Menu extends Component {
 				<br/>
 				<h4 className="heading">Drinks</h4>
 				{drink_items}
-				<br/>
-		        	{this.state.showmessage? view_message:view_cart}
+
+				<div id="fb-root"></div>
+                <script async defer crossorigin="anonymous" src="https://connect.facebook.net/zh_CN/sdk.js#xfbml=1&version=v7.0" nonce="NhYgDMHv"></script> 
+
+				<div class="fb-comments" data-href="http://localhost:3000/menu" data-numposts="5" data-width=""></div>
+
             </div>
         );
     }
