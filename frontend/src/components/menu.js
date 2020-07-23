7a5e8a901cf39e778c89f09419958606afdf058f
persdre <persdre@gmail.com>
2020-07-23 22:23:04 +0800

    Update menu.js

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -1,9 +1,16 @@
-import { Button, Modal, Table} from 'react-bootstrap';
+import { Button, Modal, Table, Badge } from 'react-bootstrap';
 import React, { Component } from 'react';
 import MetaTags from 'react-meta-tags';
 import { connect } from 'react-redux';
 import './menu.css'
+import 'font-awesome/css/font-awesome.min.css';
 
+// import NotificationBadge from 'react-notification-badge';
+// import {Effect} from 'react-notification-badge';
+
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css' 
+let status =false
 class Menu extends Component {
  	constructor(props){
  		super(props);
@@ -21,6 +28,8 @@ class Menu extends Component {
  			showmessage: false,
  			message: '',
  			emptycart: false,
+ 			count: 0,
+ 			visible: false,
  		};
  	}
  	handleClose() {
@@ -31,7 +40,7 @@ class Menu extends Component {
 	   this.setState({ show: true });
 
 	}
-	placeOrder(event,email, number){
+	placeOrder(event,name, email, number){
 		event.preventDefault();
 		
 		
@@ -49,9 +58,9 @@ class Menu extends Component {
 		else{
 
 			let o = {orderID: Math.floor(Math.random() * 1000000000000), customer_email:email, 
-				customer_number:number, restaurant_name: this.state.rest,
-				items:this.state.cart, del_location: loc, status: "pending", 
-				instructions: inst}
+				customer_number:number, customer_name:name, restaurant_name: this.state.rest,
+				items:this.state.cart, del_location: loc, del_time: "N/A", status: "pending", 
+				instructions: inst, rating: -1}
 			// console.log(o)
 			// this.setState({show:false});
 			fetch('/placeorder', {
@@ -68,7 +77,7 @@ class Menu extends Component {
 			       	this.setState({showmessage:true, message: response, cart: [], location: '', instructions: '', total:0})
 			    }); 
 		    })
-
+		this.setState({count: 0})
 		}
 
 	}
@@ -94,14 +103,26 @@ class Menu extends Component {
 		       	this.setState({menu: t})
 		    }); 
 	    })
+	    if(this.props.auth.isAuthenticated){
+	    	if (this.props.auth.user.user_type==="customer"){
+		    	status=true
+	    	}
+	    }
 	}
 
 	addToCart(e,id,name,price, cat)
-	{	e.preventDefault()
+	{	
+		e.preventDefault()
 		this.state.cart.push({item_id: id, name: name, price: price, category: cat, 
 			restaurant_name: this.state.rest})
 		let updatedPrice= this.state.total + price
-		this.setState({total: updatedPrice})
+		let tempCount = this.state.count
+		tempCount += 1
+		this.setState({total: updatedPrice, count:tempCount})      
+	    toast.error(name + " added to cart!", {
+	        position: toast.POSITION.TOP_RIGHT,
+	    });
+
 	}
 
 	removeItemFromCart(e,item_id)
@@ -117,14 +138,28 @@ class Menu extends Component {
 		}
 		let new_price = this.state.total- remPrice
 		this.state.cart.splice(z,1)
-		this.setState({total: new_price})
+		let tempCount = this.state.count
+		tempCount -= 1
+		this.setState({total: new_price, count:tempCount})
 		console.log(this.state.cart)
 	}
 
     render() {
 
+	    toast.configure({
+  			autoClose: 1800,
+		 	draggable: false,
+		});
+
+		if (this.state.count > 0){
+			this.state.visible = true
+		}
+		else{
+			this.state.visible = false
+		}
+
     	if (this.state.emptycart === true){
-    		alert("Your calorie cart is empty. Please click the + symbol next to an item to add it to your cart.")
+    		alert("Your shopping cart is empty. Please click the + symbol next to an item to add it to your cart.")
     		this.state.emptycart = false
     	}
 
@@ -142,30 +177,49 @@ class Menu extends Component {
     		}
     	}
 
+    	var promo = []
+		for (var j = this.state.menu.length - 1; j >= 0; j--) {
+    		if (this.state.menu[j].category==="Promotions"){
+    			promo.push(this.state.menu[j])
+    		}
+    	}
+
     	let c = this.state.cart
 
     	const food_items = food.map((d,i)=> 
-    		<div id="lol">
-	    		<div id= "items" key={i}> 
+    		<div id="lol" key={i}>
+	    		<div id= "items" > 
 	    		    <div>&nbsp; {d.name} </div>
 	    			<div className="spacer"/>
-		    		<div> Cal.{d.price} &nbsp; </div>
+		    		<div> Rs.{d.price} &nbsp; </div>
 	    		</div>
 		 	<button id='b2' title="Add to cart" onClick = {(e)=> {this.addToCart(e,d.item_id,d.name,d.price, d.category)}}> &nbsp; + &nbsp; &nbsp; </button>  
     		</div>
     	)
 
     	const drink_items = drinks.map((d,i)=> 
-    		<div id="lol">
-	    		<div id= "items" key={i}> 
+    		<div id="lol" key={i}>
+	    		<div id= "items" > 
 	    		    <div>&nbsp; {d.name} </div>
 	    			<div className="spacer"/>
-		    		<div> Cal.{d.price} &nbsp; </div>
+		    		<div> Rs.{d.price} &nbsp; </div>
 	    		</div>
 		 	<button id='b2' title="Add to cart" onClick = {(e)=> {this.addToCart(e,d.item_id,d.name,d.price, d.category)}}> &nbsp; + &nbsp; &nbsp; </button>  
     		</div>
     	)
     	
+    	const promo_items = promo.map((d,i)=> 
+    		<div id="lol" key={i}>
+	    		<div id= "items" > 
+	    		    <div>&nbsp; {d.name} </div>
+	    			<div className="spacer"/>
+		    		<div> Rs.{d.price} &nbsp; </div>
+	    		</div>
+		 	<button id='b2' title="Add to cart" onClick = {(e)=> {this.addToCart(e,d.item_id,d.name,d.price, d.category)}}> &nbsp; + &nbsp; &nbsp; </button>  
+    		</div>
+    	)
+
+
     	const cart_table = c.map((d,i)=>
     		<tr>
 	    		<td> {d.item_id} </td>
@@ -178,7 +232,7 @@ class Menu extends Component {
     	const view_cart = (
     	<Modal show={this.state.show} animation='true' onHide={this.handleClose}>	
     		<Modal.Header closeButton>
-	            <Modal.Title>Calorie Cart for {this.state.rest}</Modal.Title>
+	            <Modal.Title>Shopping cart for {this.state.rest}</Modal.Title>
 	          </Modal.Header>
 	          <Modal.Body>
 
@@ -187,25 +241,25 @@ class Menu extends Component {
 		          		<tr>
 			          		<th>Item ID</th>
 			          		<th>Name</th>
-			          		<th>Calorie</th>
+			          		<th>Price</th>
 		          		</tr>
 		          	</thead>
 		          		<tbody>
 			          		{cart_table}
 			          	<tr> 
 			          		<td> </td>
-			          		<td> <b>Total Calories</b> </td>
+			          		<td> <b>Total Order Price</b> </td>
 			          		<td> <b>{this.state.total}</b> </td>
 			          	</tr>
 						</tbody>
 		          </Table>
 
-		         <form onSubmit={(e)=>{this.placeOrder(e,this.props.auth.user.email,this.props.auth.user.number)}}>
+		         <form onSubmit={(e)=>{this.placeOrder(e,this.props.auth.user.name,this.props.auth.user.email,this.props.auth.user.number)}}>
 	                <label>
-	                    Breakfast/Lunch/Dinner &nbsp; &nbsp; &nbsp;  
+	                    Delivery Location &nbsp; &nbsp; &nbsp;  
 	                    <input
 	                    type="text"
-	                    placeholder=""
+	                    placeholder="Location to deliver to"
 	                    name="location"
 	                    required
 	                    />
@@ -213,21 +267,18 @@ class Menu extends Component {
 	                <br/>
 
 	                <label>
-	                	Ideal Calorie Intake  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+	                	Special Instructions &nbsp;
 	                    <input
 	                    type="text"
-	                    placeholder=""
+	                    placeholder="Instructions"
 	                    name="instruction"
 	                    // value = {this.state.instructions}
 	                    />
 	                </label>
 	                <br/>
 					<Button variant="danger" type="submit">
-					 Document All
+					  Place Order
 					</Button>
-					<label>
-					  &nbsp;&nbsp;Please login first.
-	                </label>
 				</form>
 
 	          </Modal.Body>
@@ -240,7 +291,7 @@ class Menu extends Component {
     	const view_message = (
     		<Modal show={this.state.show} animation='true' onHide={this.handleClose}>	
     		<Modal.Header closeButton>
-	            <Modal.Title>Calorie Cart for {this.state.rest}</Modal.Title>
+	            <Modal.Title>Shopping cart for {this.state.rest}</Modal.Title>
 	          </Modal.Header>
 	          <Modal.Body>
 
@@ -253,26 +304,52 @@ class Menu extends Component {
 	    </Modal>
 	    ) 
 
-	    
+	    const cartCount = (
+    		  <Badge pill id="badge">
+			    {this.state.count}
+			  </Badge>
+	    )
+
+    
         return (
+        	<div className="stuff">
+        	{!status? <h1 id="heading">Please Login as a Customer to view Menu</h1>
+        		:
             <div id="bg">
 				<MetaTags>
 	                <meta charSet="utf-8" name="viewport" content="width=device-width, initial-scale=1.0"/>
 	                <meta name="theme-color" content="#B02737"/>
 	            </MetaTags>
-            <h1 id="heading">{this.state.rest}
-            	<Button variant="danger" title="View shopping cart" className = "VC" onClick={this.handleShow}>
-		        </Button>
+            <h1 id="headingRes">{this.state.rest}
+            	<Button variant="danger" id="cartstyle" onClick={this.handleShow}>
+            		<span id="spann" title="View shopping cart" className="fa fa-3x fa-shopping-cart"></span> 
+            		<h6 id="cartHeading">Shopping Cart</h6>
+            		{this.state.visible ? cartCount : <div></div> }
+{/*        		  	<NotificationBadge 
+        		  		count={this.state.count}
+        		  		effect={Effect.SCALE}
+        		  		style={{color: 'red', backgroundColor:'black'}}
+        		  		frameLength={70.0}
+        		  	/>
+*/}				</Button>
 		    </h1>
-		    <h5 className="heading1"> select an item to document its calories  </h5>
+		    <h5 id="heading2"> Select an Item to add to your Shopping Cart </h5>
 
             <br/>
-	            <h4 className = "heading">Food</h4>
+	            <h4 id = "heading2">Food</h4>
 				{food_items}
 				<br/>
-				<h4 className="heading">Drinks</h4>
+				<h4 id="heading2">Drinks</h4>
 				{drink_items}
+				<br/>
+	            <h4 id = "heading2">Promotions</h4>
+				{promo_items}
+				<br/>
+
+		        	{this.state.showmessage? view_message:view_cart}
             </div>
+        	}
+        </div>
         );
     }
 }
